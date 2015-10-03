22c1bad5f83331bf07c1921ae3c565bbbc7f66b3
Ján Zázrivec <jan.zazrivec@gmail.com>
2015-10-03 13:36:41 +0200

    mesos version 0.24.1 postfix hardcoded on docker image definition

--- project/DockerProperties.scala
+++ project/DockerProperties.scala
@@ -16,7 +16,7 @@ object DockerProperties extends BuildConf {
     Cmd("RUN", "apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF"),
     Cmd("RUN", "echo \"deb http://repos.mesosphere.io/ubuntu trusty main\" | tee /etc/apt/sources.list.d/mesosphere.list"),
     Cmd("RUN", "/usr/bin/apt-get -y update --fix-missing"),
-    Cmd("RUN", s"/usr/bin/apt-get -y install mesos=$mesosVersion-1.0.ubuntu1404"), //ubuntu 14.04 is base for java:latest → https://github.com/dockerfile/ubuntu/blob/master/Dockerfile
+    Cmd("RUN", s"/usr/bin/apt-get -y install mesos=$mesosVersion-0.2.35.ubuntu1404"), //ubuntu 14.04 is base for java:latest → https://github.com/dockerfile/ubuntu/blob/master/Dockerfile
     Cmd("ENV", s"MESOS_JAVA_NATIVE_LIBRARY /usr/local/lib/libmesos-$mesosVersion.so"),
     Cmd("ENV", s"MESOS_LOG_DIR /var/log/mesos")
   )
